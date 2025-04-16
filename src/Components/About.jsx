63c4eb7283f3fe2766070786eaa4dc622edd3279
Elias Isac <115378394+EliasIsac1996@users.noreply.github.com>
2025-04-16 13:59:59 +0000

    PrimeirosPassosComgithubestudent

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,7 +24,7 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "Eu sou estudante em engenharia da computação, fascinado por tecnologia, estou estudando para ser Desenvolvedor web e móvel";
 
 /**
  * List of some of skills or technologies you work on, are learning,
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "Eu sou detalhista e gosto de utilizar conceitos de pensamento computacional para resolução de problemas.";
 
 const About = () => {
   return (
